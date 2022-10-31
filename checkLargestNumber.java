d8d6fc310a78c611e87216799f4855b22682ebb4
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-10-31 19:09:33 +0530

    To check largest among 3 numbers

--- checkLargestNumber.java
+++ checkLargestNumber.java
@@ -3,7 +3,7 @@ package com.blz.day5;
 import java.util.Scanner;
 
 public class checkLargestNumber {
-	public static void main(String[] args) {
+     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
      System.out.println("To calculate largest number among 3  numbers");
      System.out.println("Enter any 3 integers");
@@ -12,5 +12,5 @@ public class checkLargestNumber {
      int num3 = sc.nextInt();
      int large = (num1 > num2 ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3));
      System.out.println("Largest number among 3 numbers is :  "+large);
-	}
+     }
 }
